23053cbcb2a9c68f6b8443ccef3c935bcea75ac4
Scott Deardorff <sdeardorff@bitcentral.com>
2020-08-16 19:31:19 -0700

    Updates after debugging

--- cloudformation/deploy_cert-update.sh
+++ cloudformation/deploy_cert-update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # This script would be called by your continuous delivery application, e.g. Ansible, Octopus, etc.
+# Cut paste/glue/hack/gouge - you know the drill
 ENVIRONMENT="dev"
 RELEASE="0.1"     # Update this to force lambda code to update when no other config changes
 CUSTOMER="atb"
@@ -7,20 +8,24 @@ APP="web"
 MODULE="ssl"
 COMPONENT="cert-update"
 REGION="us-east-1"
-HOSTED_ZONE_ID="Z04156561UJTD1JXFR76"
-PARENT_IAM_STACK="${CUSTOMER}-${APP}-${MODULE}-cert_iam-${ENVIRONMENT}"
-SCRIPT_PREFIX="cert_lambda/${RELEASE}"
+HOSTED_ZONE_ID="<YOUR_HOSTED_ZONE_ID>"
+PARENT_IAM_STACK="${CUSTOMER}-${APP}-${MODULE}-cert-iam-${ENVIRONMENT}"
+SCRIPT_PREFIX="cert_lambda/${RELEASE}" # This is where the magic of updating code only happens
 
 
 STACK_NAME="${CUSTOMER}-${APP}-${MODULE}-${COMPONENT}-${ENVIRONMENT}"
 PRIVATE_BUCKET="${CUSTOMER}-${APP}-${MODULE}-private-${ENVIRONMENT}"
 
+# The buildy bits
+rm -f acme-dns-route53.git
 git clone git@github.com:scottdear/acme-dns-route53.git
 cd acme-dns-route53
 env GOOS=linux GOARCH=amd64 go build
 
+rm -f acme-dns-route53.zip
 zip -j acme-dns-route53.zip acme-dns-route53
 
+# The deployment bits
 aws s3 cp acme-dns-route53.zip "s3://${PRIVATE_BUCKET}/${SCRIPT_PREFIX}/acme-dns-route53.zip"
 
 cd ..
@@ -32,8 +37,10 @@ aws cloudformation deploy --stack-name $STACK_NAME --region $REGION --capabiliti
 	    customer=$CUSTOMER \
 	    environment=$ENVIRONMENT \
 	--parameter-overrides \
-        ParentIAMStack=PARENT_IAM_STACK \
+	    EnvironmentName=$ENVIRONMENT \
+        ParentIAMStack=$PARENT_IAM_STACK \
 	    PrivateBucket=$PRIVATE_BUCKET \
-	    ScriptBucket=$SCRIPT_BUCKET \
-	    ScriptPrefix=$SCRIPT_PREFIX
+	    ScriptBucket=$PRIVATE_BUCKET \
+	    ScriptPrefix=$SCRIPT_PREFIX \
+	    HostedZoneID=$HOSTED_ZONE_ID
 
